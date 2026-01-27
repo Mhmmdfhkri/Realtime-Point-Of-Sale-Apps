a37f062aea99d4ae235f98d491e3a3aadce41692
Mhmmdfhkri <mufik2002@gmail.com>
2026-01-28 07:39:17 +0800

    feat: add form validation using Zod and React Hook Form

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import Login from "./_components/login";
+
+
+export const metadata = {
+    title: "Fikri Cafe | Login",
+};
+
+
+export default function LoginPage() {
+    return <Login />;    
+}
