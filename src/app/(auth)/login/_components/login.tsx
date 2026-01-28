20d47994291c6539dacae4541e29b06d09b1014b
Mhmmdfhkri <mufik2002@gmail.com>
2026-01-28 11:42:56 +0800

    feat: add Toaster Notification

--- src/app/(auth)/login/_components/login.tsx
+++ src/app/(auth)/login/_components/login.tsx
@@ -20,6 +20,7 @@ import { startTransition, useActionState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { login } from "../actions";
 import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
 
 export default function Login() {
   const form = useForm<LoginForm>({
@@ -44,11 +45,14 @@ export default function Login() {
 
   useEffect(() => {
 if(loginState?.status === "error") {
+  toast.error("Login Failed", {
+    description: loginState.errors._form?.[0],
+  })
   startTransition(() => {
     loginAction(null)
   })
 }
-  }, [loginState])
+  }, [loginState]);
 
   console.log(loginState);
 
