43aa3ed93f0e5be2d8de9bdd2fd1b318e867e42c
Mhmmdfhkri <mufik2002@gmail.com>
2026-02-01 22:33:23 +0800

    feat: add Input File Handle

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions:{
+      bodySizeLimit: "10mb"
+    },
+  },
+  devIndicators: false,
+  images: {
+    domains: ["https://iadzewbkgvosisvpxvtq.supabase.co"],
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "iadzewbkgvosisvpxvtq.supabase.co",
+      port: "",
+      pathname: "/**",
+    }]
+  }
 };
 
 export default nextConfig;
