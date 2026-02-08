92c723aeab19b2e70d7b536e9810b72791a58383
Mhmmdfhkri <mufik2002@gmail.com>
2026-02-08 16:23:45 +0800

    feat: generate payment url

--- src/app/(dashboard)/order/actions.ts
+++ src/app/(dashboard)/order/actions.ts
@@ -5,6 +5,8 @@ import { FormState } from "@/types/general";
 import { Cart, OrderFormState } from "@/types/order";
 import { orderFormSchema } from "@/validations/order-validation";
 import { redirect } from "next/navigation";
+import midtrans from "midtrans-client";
+import { environment } from "@/configs/environment";
 
 export async function createOrder(
   prevState: OrderFormState,
@@ -144,12 +146,14 @@ export async function updateStatusOrderItem(
 ) {
   const supabase = await createClient();
 
-  const { error } = await supabase.from("orders_menus").update({
-    status: formData.get("status"),
-  }).eq("id", formData.get("id"));
+  const { error } = await supabase
+    .from("orders_menus")
+    .update({
+      status: formData.get("status"),
+    })
+    .eq("id", formData.get("id"));
 
-
-  if(error){
+  if (error) {
     return {
       status: "error",
       errors: {
@@ -160,6 +164,64 @@ export async function updateStatusOrderItem(
   }
 
   return {
-    status: "success"
-  }
+    status: "success",
+  };
 }
+
+
+
+
+export async function generatePayment(
+  prevState: FormState,
+  formData: FormData,
+) {
+  const supabase = await createClient();
+  const orderId = formData.get("id");
+  const gross_amount = formData.get("gross_amount");
+  const customerName = formData.get("customer_name");
+
+  const snap = new midtrans.Snap({
+    isProduction: false,
+    serverKey: environment.MIDTRANS_SERVER_KEY!,
+  });
+
+  const parameter = {
+    transaction_details: {
+      order_id: `${orderId}`,
+      gross_amount: parseFloat(gross_amount as string),
+    },
+    customer_details: {
+      first_name: customerName,
+    },
+  };
+
+  const result = await snap.createTransaction(parameter);
+
+  if (result.error_messages) {
+    return {
+      status: "error",
+      errors: {
+        ...prevState,
+        _form: [result.error_messages],
+      },
+      data: {
+        payment_token: "",
+      },
+    };
+  }
+
+  await supabase
+    .from("orders")
+    .update({ payment_token: result.token })
+    .eq("order_id", orderId);
+
+
+    return {
+      status: "success",
+      data: {
+        payment_token: `${result.token}`,
+      }
+    }
+}
+
+
